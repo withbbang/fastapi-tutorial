e87fc81f2d5c69aa6e24409491425599c4d25c6f
김영선 <withbbang@gmail.com>
2023-08-11 13:56:23 +0900

    Docs: git commit message template 추가 Chore: 경로 파라미터, 필수 파라미터 공부

--- fastapi-tutorial/main.py
+++ fastapi-tutorial/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 
 @app.get("/")
@@ -26,3 +33,36 @@ async def read_item(item_id: str, q: Union[str, None] = None, short: bool = Fals
 
     print(item) # {'item_id': 'foo', 'q': 'good'}
     return item
+
+# /users/bread/items/macho?short=1
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+
+    if q:
+        item.update({"q": q})
+
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# 필수 쿼리 파라미터
+# /items/foo-item -> 404
+# /items/foo-item?needy=sooooneedy -> 200
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str): # needy의 타입을 선언할 경우, str 타입의 needy란 변수는 꼭 있어야한다.
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+@app.post("/datas/{item_id}")
+async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"item_id": item_id, **item.dict()}
+    print(result)
+    if q:
+        result.update({"q": q})
+    return result
