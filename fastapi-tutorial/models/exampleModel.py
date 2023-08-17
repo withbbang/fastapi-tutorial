73c62d53180ca0d8cf68579c12fe7b749b1f55f9
김영선 <withbbang@gmail.com>
2023-08-17 14:59:39 +0900

    TEST: Docs 페이지에 example data 표시 확인

---
+++ fastapi-tutorial/models/exampleModel.py
@@ -0,0 +1,21 @@
+from typing import Union
+from pydantic import BaseModel
+
+class ExampleModel(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
--- fastapi-tutorial/routers/body.py
+++ fastapi-tutorial/routers/body.py
@@ -7,6 +7,7 @@ from models.field import FieldTest
 from models.list import Lists
 from models.set import Sets
 from models.subModel import SubModel
+from models.exampleModel import ExampleModel
 
 router = APIRouter(
     prefix="/router",
@@ -16,7 +17,7 @@ router = APIRouter(
 async def body():
     return {"message": "hello"}
 
-# Annotated 사용시 검증 조건을 쉽게 추가할 수 있다.
+# Annotated 사용시, 검증 조건을 쉽게 추가할 수 있다.
 @router.post("/items/{item_id}")
 async def update_item(
     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
@@ -56,17 +57,26 @@ async def update_item(item_id: int, item: FieldTest):
     results = {"item_id": item_id, "item": item}
     return results
 
+# body 내에 list 형태의 파라미터
 @router.post("/lists/{item_id}")
 async def update_item(item_id: int, lists: Lists):
     results = {"item_id": item_id, "lists": lists}
     return results
 
+# body 내에 특정 타입의 list 형태 파라미터
 @router.post("/sets/{item_id}")
 async def update_item(item_id: int, sets: Sets):
     results = {"item_id": item_id, "sets": sets}
     return results
 
+# body 내에 특정 타입의 list 형태 파라미터
 @router.post("/sub-model/{item_id}")
 async def update_item(item_id: int, subModel: SubModel):
     results = {"item_id": item_id, "subModel": subModel}
+    return results
+
+# docs에 모델 예시 표시 기능
+@router.post("/example-model/{item_id}")
+async def update_item(item_id: int, exampleModel: ExampleModel):
+    results = {"item_id": item_id, "exampleModel": exampleModel}
     return results
